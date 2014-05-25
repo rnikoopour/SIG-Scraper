a5a8e0d6afe3dcbd7efeac5ed871680c469712f6
deadheartsbeat <keepitreza2@gmail.com>
2014-05-25 13:21:44 -0700

    Basic search functionality works.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-SIGSCRAPE
+SIG-Scrape
 =========
+SIG-Scrape can:
 
 Scrape the homepage of SIG.  Send an email with the matching results as the body.
+Search for a specific item.  Displays the results in the console and writes them to results.txt
 
 Must have wanted.txt in the same directory
 Must have lastread.txt in the same directory
@@ -12,3 +14,7 @@ This command scrapes the homepage
 python soitgoes.py scrape SIG-username SIG-password email-username email-pass
 --
 
+This command searches SIG
+==
+python soitgoes.py search SIG-username SIG-password "Search Term"
+==
--- soitgoes.py
+++ soitgoes.py
@@ -3,7 +3,12 @@ import string, email
 import sys, thread, time
 from email.mime.text import MIMEText
 
+# This will hold our cookie gloablly
+sig_cookies = None
+
 def login_to_sig(sig_un, sig_pass):
+    global sig_cookies
+    
     # Login URL and parameters for SIG
     LOGIN_URL = 'https://soitgo.es/?i=redirect&r=YToxOntzOjE6ImkiO3M6NDoibWFpbiI7fQ=='
     PARAMS = {'user':sig_un, 'pass':sig_pass}
@@ -11,12 +16,16 @@ def login_to_sig(sig_un, sig_pass):
     # Get homepage to get session cookie
     home_req = requests.get('https://soitgo.es')
     
+    # Store session cookie for later use in global variabele
+    sig_cookies = home_req.cookies
+    
     # Log into the website
-    login_req = requests.post(LOGIN_URL, data=PARAMS, cookies=home_req.cookies)
+    login_req = requests.post(LOGIN_URL, data=PARAMS, cookies=sig_cookies)
     return login_req
     
 def scrape(sig_un, sig_pass, email_un, email_pass):
-    print "Scraping!"
+    print "\nScraping!\n"
+    
     # Variable to control writing to first found file
     first_title_saved = False
     
@@ -27,10 +36,10 @@ def scrape(sig_un, sig_pass, email_un, email_pass):
     last_read = [item for item in open('lastread.txt', 'r')]
     last_read_found = False
     
-    login_req = login_to_sig(sig_un, sig_pass)
+    home_page = login_to_sig(sig_un, sig_pass)
 
     # Create a BeautifulSoup object using the HTML contents after logging in
-    page = bs4.BeautifulSoup(login_req.content)
+    page = bs4.BeautifulSoup(home_page.content)
     
     # Create a list of wanted items from wanted.txt
     wanted = [item for item in open('wanted.txt', 'r')]
@@ -47,9 +56,9 @@ def scrape(sig_un, sig_pass, email_un, email_pass):
             # Save the first non-Featured link we find
             if first_title_saved == False and category != 'Featured':
                 # Open the file, write the title, the close
-                write_title = open('lastread.txt', 'w')
-                write_title.write(title)
-                write_title.close()
+                lastread_file = open('lastread.txt', 'w')
+                lastread_file.write(title)
+                lastread_file.close()
                 first_title_saved = True
                 
             # Check to see if the current title hasn't been seen before
@@ -59,7 +68,9 @@ def scrape(sig_un, sig_pass, email_un, email_pass):
                     # Loop through wanted items
                     for item in wanted:
                         # Remove the newline at the end of string
-                        i = item[:-1]
+                        i = item.strip('\n')
+                        # Remove any leading or trailing spaces
+                        i = i.strip()
                         # See if i is a substring in title
                         if re.search(i.lower(), title.lower()):
                             # Add it to the body of the email.
@@ -90,11 +101,63 @@ def scrape(sig_un, sig_pass, email_un, email_pass):
     # Close server connection
     server.quit()
     
-    print "Done Scraping!"
+    print "\nDone Scraping!\n"
+    
+def search(sig_un, sig_pass, search_term):
+    print "\nSearching!\n"
+    
+    # Get global reference so we can use the cookie provided to us
+    global sig_cookies
+    
+    # Lets us know if we find results
+    result_found = False
+    
+    # Log into the website
+    login_to_sig(sig_un, sig_pass)
+
+    # Set up the search url 
+    search_url = 'https://soitgo.es/?i=' + search_term + '&do=search'
+    
+    # Get search results page and turn it into Beautiful Soup
+    search_results = requests.get(search_url, cookies=sig_cookies)
+    page = bs4.BeautifulSoup(search_results.content)
+    
+    # This grabs the blank row that is at the top of the search area
+    blank_row = page.find_all('div', class_='row header')
+    
+    # Open searchresults.txt in write mode. This overwrites previous search results.
+    results_file = open('results.txt', 'w')
+    results_file.write('Category\tTitle\t\t\t\t\t\t\t\tLink\n')
+    
+    # Loop through all the rows
+    for row in page.find_all('div', class_='row'):
+        # If we found the blank row go ahead and go to the next item in the list
+        if row in blank_row:
+            pass
+        else:
+            # Result was found
+            result_found = True
+            # Get the category, title, and query string for link
+            category = row.find(class_='category').text
+            title = row.find(class_='title').text
+            link_param = row.find_all('a')[1].get('href')
+            if len(title) < 60:
+                title = title + ' ' * (60 - len(title))
+            print title[:60] + ' is on SIG: www.soitgo.es' + link_param
+            results_file.write(category + '\t\t' + title[:55] + '\t\twww.soitgo.es' + link_param + '\n')
+            
+    if result_found == False:
+        print "No Results Found."
+        results_file.write("\nNo Results Found.\n")
+    
+    results_file.close()
+    print "\nDone Searching. Information stored in results.txt\n"
 
 def main(argv):
     if argv[0] == "scrape":
         scrape(argv[1], argv[2], argv[3], argv[4])
+    elif argv[0] == "search":
+        search(argv[1], argv[2], argv[3])
 
 if __name__ == '__main__':
     #Pass command line args to main
